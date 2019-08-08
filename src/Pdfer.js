2a546877bcecf4c73147094c1df9db1c3869a7c3
A Groshek <amy@amygroshek.com>
2019-08-08 14:24:18 -0500

    Bar chart pages layout

--- src/Pdfer.js
+++ src/Pdfer.js
@@ -18,6 +18,21 @@ const _ats_hrl = [
   }
 ];
 
+const _coh_dri = [
+  {
+    range: [Number.NEGATIVE_INFINITY, -0.0249999],
+    text: 'declining'
+  },
+  {
+    range: [-0.025, 0.025],
+    text: 'relatively stable'
+  },
+  {
+    range: [0.02500001, Number.POSITIVE_INFINITY],
+    text: 'improving'
+  }
+];
+
 const _ats_ab = [
   {
     range: [Number.NEGATIVE_INFINITY, -0.0000001],
@@ -26,6 +41,50 @@ const _ats_ab = [
   {
     range: [0, Number.POSITIVE_INFINITY],
     text: 'above'
+  }
+];
+
+const _ats_diff_hl = [
+  {
+    range: [Number.NEGATIVE_INFINITY, -0.0000001],
+    text: 'lower'
+  },
+  {
+    range: [0, Number.POSITIVE_INFINITY],
+    text: 'higher'
+  }
+];
+
+const _coh_diff_id = [
+  {
+    range: [Number.NEGATIVE_INFINITY, -0.0000001],
+    text: 'decreased'
+  },
+  {
+    range: [0, Number.POSITIVE_INFINITY],
+    text: 'increased'
+  }
+];
+
+const _coh_diff_ml = [
+  {
+    range: [Number.NEGATIVE_INFINITY, -0.0000001],
+    text: 'less'
+  },
+  {
+    range: [0, Number.POSITIVE_INFINITY],
+    text: 'more'
+  }
+];
+
+const _coh_id = [
+  {
+    range: [Number.NEGATIVE_INFINITY, -0.0000001],
+    text: 'decreased'
+  },
+  {
+    range: [0, Number.POSITIVE_INFINITY],
+    text: 'increased'
   }
 ];
 
@@ -60,18 +119,82 @@ const _ats_vfb = [
   }
 ];
 
-const _ats_ab = [
+const _grd_hrl = [
   {
-    range: [Number.NEGATIVE_INFINITY, -0.0000001],
-    text: 'below'
+    range: [Number.NEGATIVE_INFINITY, 0.649999],
+    text: 'lower than'
   },
   {
-    range: [0, Number.POSITIVE_INFINITY],
-    text: 'above'
+    range: [0.65, 1.35],
+    text: 'roughly'
+  },
+  {
+    range: [1.350001, Number.POSITIVE_INFINITY],
+    text: 'higher than'
   }
 ];
 
+const _grd_lsm = [
+  {
+    range: [Number.NEGATIVE_INFINITY, -0.000001],
+    text: 'less each grade than'
+  },
+  {
+    range: [0, 0],
+    text: 'about the same each grade as'
+  },
+  {
+    range: [0.000001, Number.POSITIVE_INFINITY],
+    text: 'more each grade than'
+  }
+];
+
+/**
+ * Returns the value rounded to the provided number of decimal
+ * places.
+ */
+const formatNumber = (val, decimals = 2) => {
+  if (!val && val !== 0) { return 'N/A' }
+  const factor = Math.pow(10, decimals);
+  return Math.round(val*factor)/factor;
+}
+/**
+ * Returns a percent string from the provided value
+ * @param {number} v
+ */
+const formatPercent = (v, decimals = 0) => {
+  if (!v && v !== 0) { return 'N/A' }
+  return formatNumber(v * 100, decimals)
+}
+/**
+ * Returns a percent string of how far the provided value
+ * is from the provided `from` value. (used for learning rates)
+ * @param {number} v the value to format
+ * @param {number} from the point of reference to determine what the % diff is
+ */
+const formatPercentDiff = (v, from = 1) => {
+  if (!v && v !== 0) { return 'N/A' }
+  return formatPercent(v - from);
+}
+
 class Pdfer {
+
+  async getPercentDiffBoilerplate(v, from = 1) {
+    console.log('getPercentDiffBoilerplate()');
+    if (!v && v !== 0) { return 'N/A' }
+    const percent = formatPercent(v - from);
+    console.log(percent);
+    switch (true) {
+      case (percent < 0):
+        return (percent * -1) + '% less each grade than';
+      case (percent === 0):
+        return 'about the same each grade as';
+      case (percent > 0):
+        return percent + '% more each grade than';
+      default:
+        return 'about the same each grade as';
+    }
+  }
 
   async getFixed(value, decimals) {
     return value.toFixed(decimals);
@@ -261,8 +384,37 @@ class Pdfer {
         _verbiage.ats_diff_fixed = await this.getPos(
           await this.getFixed(jsonparse.location.diff_avg, 2)
         );
+        _verbiage.ats_diff_hl = await this.getBoilerplate(jsonparse.location.diff_avg, _ats_diff_hl);
+      }
+      _verbiage.grd_hrl = await this.getBoilerplate(jsonparse.location.all_grd, _grd_hrl);
+      _verbiage.grd_pct = await this.getPercentDiffBoilerplate(jsonparse.location.all_grd, 1);
+      if (!!jsonparse.location.diff_grd) {
+        _verbiage.grd_diff_fixed = await this.getPos(
+          await this.getFixed(jsonparse.location.diff_grd, 2)
+        );
+        _verbiage.grd_diff_hl = await this.getBoilerplate(jsonparse.location.diff_grd, _ats_diff_hl);
+      }
+      _verbiage.coh_dri = await this.getBoilerplate(jsonparse.location.all_coh, _coh_dri);
+      _verbiage.coh_id = await this.getBoilerplate(jsonparse.location.all_coh, _coh_id);
+      _verbiage.coh_grd = await this.getPos(
+        await this.getFixed(jsonparse.location.all_coh, 2)
+      );
+      if (!!jsonparse.location.diff_coh) {
+        _verbiage.coh_diff_fixed = await this.getPos(
+          await this.getFixed(jsonparse.location.diff_coh, 2)
+        );
+        _verbiage.coh_diff_id = await this.getBoilerplate(jsonparse.location.diff_coh, _coh_diff_id);
+        _verbiage.coh_diff_ml = await this.getBoilerplate(jsonparse.location.diff_coh, _coh_diff_ml);
       }
       jsonparse.verbiage = _verbiage;
+      const _css = {}; // JSON object for CSS attribs needed by template
+      _css.avg_left = '100px';
+      _css.avg_top = '100px';
+      _css.grd_left = '100px';
+      _css.grd_top = '100px';
+      _css.coh_left = '100px';
+      _css.coh_top = '100px';
+      jsonparse.css = _css;
       console.log(jsonparse);
       // Fetch the template.
       // console.log('template string');
