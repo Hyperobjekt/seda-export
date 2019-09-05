632d0b8408193c9fc2242a4a19bc351865f04221
Lane Olson <laneolson@gmail.com>
2019-09-04 19:54:11 -0600

    add server to accept POST requests and generate PDF

---
+++ server.js
@@ -0,0 +1,47 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const Pdfer = require('./src/Pdfer.js');
+const fs = require('fs');
+var cors = require('cors')
+
+const app = express()
+app.use(cors())
+app.use(bodyParser.json())
+
+const port = 3001
+
+const getValidationErrors = (data) => {
+  if (!data || !data.location) 
+    return "no location data provided"
+  if (!data.region) 
+    return "no region provided"
+  if (!data.url) 
+    return "no URL provided"
+  return null;
+}
+
+/**
+ * Post route, accepts location JSON and sends PDF
+ * file if the data is valid.  Otherwise, sends error.
+ */
+app.post('/', async function(req, res, next) {
+  const pdfData = req.body;
+  const error = getValidationErrors(pdfData);
+  if (error) {
+    res.send(error)
+  } else {
+    var filename = pdfData.location.id + ".pdf"; 
+    filename = encodeURIComponent(filename);
+
+    var pdfer = new Pdfer();
+    await pdfer.pdf(pdfData, './src/assets/templates', './output/' + filename);
+    var stream = fs.createReadStream('./output/' + filename);
+
+    res.setHeader('Content-disposition', 'inline; filename="' + filename + '"');
+    res.setHeader('Content-type', 'application/pdf');
+  
+    stream.pipe(res);
+  }
+})
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
--- src/ExportPDF.js
+++ src/ExportPDF.js
@@ -5,7 +5,10 @@ console.log('exportPDF called.');
 console.log('data: ' + args.data);
 console.log('templates: ' + args.templates);
 console.log('output: ' + args.output);
-
+ 
 // Init PDF generation
 var pdfer = new Pdfer();
-pdfer.pdf(args.data, args.templates, args.output);
+pdfer.pdf(args.data, args.templates, args.output)
+  .then(function() {
+    process.exit()
+  })
